5234199725a374c2745db0ea03383e33dc5f547e
Raúl Rodríguez <raulrodriguezaznar@gmail.com>
2022-06-06 20:46:50 +0200

    Adding how to handle fetch errors

--- src/Home.js
+++ src/Home.js
@@ -5,22 +5,32 @@ const Home = () => {
 
     const [games, setGames] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/games')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setGames(data);
-                setIsLoading(false);
-            });
+                .then(res => {
+                    if(!res.ok){
+                        throw Error('Could not fetch the data from that resource.')
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setGames(data);
+                    setIsLoading(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsLoading(false);
+                    setError(err.message);
+                })
         }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isLoading && <div>Loading...</div>}
             {games && <GameList games={games} title="All Games!" />}
         </div>
