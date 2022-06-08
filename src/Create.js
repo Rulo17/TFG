17288854d71c1301498a02e2d2d013c98d92311c
Raúl Rodríguez <raulrodriguezaznar@gmail.com>
2022-06-08 21:25:30 +0200

    Creating form inside Create.js component

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+
 const Create = () => {
+    const [ title, setTitle ] = useState('');
+    const [ description, setDescription ] = useState('');
+    const [ author, setAuthor ] = useState('');
+
+
     return (
         <div className="create">
             <h2>Add a New Game</h2>
+            <form>
+                <label>Game title:</label>
+                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
+
+                <label>Game description:</label>
+                <textarea value={description} onChange={(e) => setDescription(e.target.value)} required />
+
+                <label>Game author:</label>
+                <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} required />
+
+                <button>Add Game</button>
+            </form>
         </div>
     );
 }
