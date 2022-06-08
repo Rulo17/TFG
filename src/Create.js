9c22d8292d2c425ab958d17be50566a4df7cca58
Raúl Rodríguez <raulrodriguezaznar@gmail.com>
2022-06-08 19:58:25 +0200

    Installing react router, using router links, and creating Create.js component

---
+++ src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+    return (
+        <div className="create">
+            <h2>Add a New Game</h2>
+        </div>
+    );
+}
+ 
+export default Create;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>Play & Share Mind Games</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{color: '#2E0249', backgroundColor: 'goldenrod', borderRadius: '8px'}}>New Game</a>
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{color: '#2E0249', backgroundColor: 'goldenrod', borderRadius: '8px'}}>New Game</Link>
             </div>
         </nav>
     );
